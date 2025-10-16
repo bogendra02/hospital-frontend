7e9a609eeadc73c7764bfa40dfe10ff5ee765268
bogendra02 <bogendra.bh@gmail.com>
2025-10-16 12:22:44 +0530

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hospital-frontend/', // 👈 very important for GitHub Pages
+  base: '/hospital-frontend/', // important!
 })
